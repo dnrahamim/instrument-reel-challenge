e153420e135e43262bf173ad4de5ae6a8895487d
Dorian Rahamim <dnrahamim@gmail.com>
2024-02-05 21:41:38 -0500

    fixes percent change calculation using more specific useEffect hook dependency on instruments

--- src/client-edit-me/edit-me/InstrumentRectangle.tsx
+++ src/client-edit-me/edit-me/InstrumentRectangle.tsx
@@ -1,39 +1,44 @@
-import { useContext, useRef } from "react";
+import { memo, useContext, useEffect, useMemo, useRef } from "react";
 import { InstrumentSymbol } from "../../common-leave-me";
 import { IconImage } from "./IconImage";
 import "./InstrumentReel.css";
 import { InstrumentContext } from "./Contexts";
 
-export const InstrumentRectangle = ({ code }: { code: InstrumentSymbol }) => {
-  const quoteRef = useRef(null as number | null);
-  const instruments = useContext(InstrumentContext);
-  const instrument = instruments.find((instrument) => instrument.code === code);
+export const InstrumentRectangle = memo(
+  ({ code }: { code: InstrumentSymbol }) => {
+    const quoteRef = useRef(null as number | null);
+    const instruments = useContext(InstrumentContext);
+    const instrument = instruments.find(
+      (instrument) => instrument.code === code
+    );
 
-  const calculatePercentChange = () => {
-    let change = 0;
-    if (instrument?.lastQuote !== quoteRef.current) {
-      debugger;
-    }
-    if (instrument?.lastQuote && quoteRef.current) {
-      change =
-        (100 * (instrument.lastQuote - quoteRef.current)) / quoteRef.current;
-    }
-    return change.toFixed(3);
-  };
-  const updateLastQuote = () => {
-    if (instrument?.lastQuote) {
-      quoteRef.current = instrument.lastQuote;
-    }
-  };
-  const percentChange = calculatePercentChange();
-  updateLastQuote();
+    const calculatePercentChange = () => {
+      let change = 0;
+      if (instrument?.lastQuote && quoteRef.current) {
+        change =
+          (100 * (instrument.lastQuote - quoteRef.current)) / quoteRef.current;
+      }
+      return change.toFixed(3);
+    };
+    const updateLastQuote = () => {
+      if (instrument?.lastQuote) {
+        quoteRef.current = instrument.lastQuote;
+      }
+    };
 
-  return instrument ? (
-    <div className="instrumentRectangle">
-      <IconImage code={instrument.code} />
-      <div className="nameBlock">{instrument.name}</div>
-      <div className="quoteBlock">{instrument.lastQuote}</div>
-      <div>{percentChange}</div>
-    </div>
-  ) : null;
-};
+    const percentChange = calculatePercentChange();
+
+    useEffect(() => {
+      updateLastQuote();
+    }, [JSON.stringify(instruments)]);
+
+    return instrument ? (
+      <div className="instrumentRectangle">
+        <IconImage code={instrument.code} />
+        <div className="nameBlock">{instrument.name}</div>
+        <div className="quoteBlock">{instrument.lastQuote}</div>
+        <div>{percentChange}</div>
+      </div>
+    ) : null;
+  }
+);
